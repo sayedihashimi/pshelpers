1f6556f3bbf7c5fca9f20b981d17ad3ade2d9de9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-10-05 13:01:31 -0700

    adding file

---
+++ find-duplicates.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+This will find duplicate files from one folder to another.
+#>
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    $leftFolder,
+    [Parameter(Mandatory=$true)]
+    $rightFolder)
+
+function Find-Duplicates{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        $leftFolder,
+        [Parameter(Mandatory=$true)]
+        $rightFolder)
+    process{
+        # compute the hash for every file under $rightFolder and place it in an array
+        $rightFiles = (Get-ChildItem $rightFolder -Recurse | Get-FileHash)
+
+        $duplicateFiles = @()
+
+        Get-ChildItem $leftFolder -Recurse | 
+            Get-FileHash | ForEach-Object {
+                $leftItem = $_
+                # see if this has is in the list above
+                $foundFiles = ($rightFiles | Where-Object { $leftItem.Hash -eq $_.Hash })
+                if($foundFiles){
+                    foreach($dup in $foundFiles){
+                        $dupEntry = @{}
+                        $dupEntry.SourceFile = $_.Path
+                        $dupEntry.DuplicateFile = $dup.Path
+                        $dupEntry.Hash = $_.Hash
+
+                        $duplicateFiles+=$dupEntry
+                    }
+                }
+            }
+        
+        $duplicateFiles
+    }
+}
+
+Find-Duplicates -leftFolder $leftFolder -rightFolder $rightFolder
